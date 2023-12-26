b01a2ec91b5dd28ad3f9fe16cd5621497cc2d91b
AlexAlmeida0 <aleexandre.almeida0@gmail.com>
2023-12-26 13:05:52 -0300

    food-commerce

--- src/components/Snacks/index.tsx
+++ src/components/Snacks/index.tsx
@@ -1,6 +1,7 @@
 import { FiPlus } from 'react-icons/fi'
 
 import { Container } from "./styles"
+import { currencyFormat } from '../../helpers/currencyformat'
 
 interface SnacksProps {
   snacks: any[]
@@ -15,7 +16,7 @@ export function Snacks({snacks}): SnacksProps {
           <img src={snack.image} alt={snack.name} />
           <p>{snack.description}</p>
           <div>
-            <strong>{snack.price}</strong>
+            <strong>{currencyFormat(snack.price)}</strong>
             <button type='button'>
               <FiPlus />
             </button>
